837b90a9a645b545562c7585c5eac1d908aeb268
Bidur Poudel <opbi2058@gmail.com>
2024-04-11 13:59:40 +0545

    search and filter works fine

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -7,8 +7,11 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 const Properties = () => {
   const [allData, setData] = useState([]);
   const [images, setImages] = useState();
+  const [filteredData, setFilteredData] = useState([]);
+  const [sort, setSort] = useState('');
   const [range, setRange] = useState('Sort By Price Range')
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [searchQuery, setSearch] = useState('')
   const priceRange = ["low to high", "high to low"]
 
   // urls
@@ -26,46 +29,97 @@ const Properties = () => {
   };
 
   useEffect(() => {
-    getData(url);
+    getData(url, searchQuery);
   }, []);
 
+  
+  const filterProperties = (query) => {
+    if (query === '') {
+      setFilteredData(allData);
+    } else {
+      const filtered = allData.filter(data =>
+        data.propertyType.propertyName.toLowerCase().includes(query) ||
+        data.address.toLowerCase().includes(query)
+      );
+      setFilteredData(filtered);
+    }
+  };
+
+
+  const handleSearchInput = (e) => {
+    setSearch(e.target.value.toLowerCase())
+    filterProperties(e.target.value.toLowerCase());
+    console.log(e.target.value)
+  }
+  const handleFilter = (filters) => {
+   
+      if (filters === 'Rooms' || filters === 'Flats' || filters === 'Shutters' || filters === 'Building' || filters === 'Land') {
+        const filtered = allData.filter(data =>
+          data.propertyType.propertyName.toLowerCase().includes(filters.toLowerCase())
+        );
+        setFilteredData(filtered); 
+    }
+  };
   return (
     <>
+      <div className="relative flex justify-center mx-[35%] mt-10 h-[2.5rem] bg-green-400 rounded-full w-[25%]"> {/*i can adjust this div now*/}
+        <form action="search" className='w-full bg-red-300 h-full rounded-full'>
+          <input type="text" className='w-full h-full border-[0.02rem]  border-black px-5 rounded-full outline-[0.1rem] outline-blue-300'
+            onChange={handleSearchInput} />
+          <button
+            className='absolute bg-gray-200 hover:bg-gray-300 right-[0.5em] top-[0.29rem] px-2 py-[0.15rem] font-semibold border border-black rounded-full'
+          >Search</button>
+        </form>
+      </div>
       <div className="flex">
-        <div className="sidebar flex flex-wrap px-2 w-80 mt-20 bg-gray-200 border border-gray-300">
-          <span className="font-semibold text-xl">Sort By:</span><br /> 
-          <p className=" text-lg font-medium mt-12 -ml-[4.5rem] pb-2">Category</p>
-          <div className="w-full flex -my-[85%]">
-            <div className="flex items-center justify-center -my-1 hover:bg-green-400 px-2 w-20 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer">Rooms</div>
-            <div className="flex items-center justify-center -my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer">Flats</div>
-            <div className="flex items-center justify-center -my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer">Shutters</div>
+        <div className="sidebar w-[25rem] h-screen bg-gray-100 border border-gray-100 drop-shadow-xl shadow-xl">
+          <div className="flex justify-center mt-2 border-b border-black text-xl font-semibold pl-4">Sort by:</div>
+          <div className="text-lg pl-2 mt-2">Category:</div>
+          <div className="flex flex-wrap categories border-b pb-4 mt-2">
+            <div
+              className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer"
+              onClick={() => handleFilter('Rooms')}
+            >
+              Rooms</div>
+
+            <div
+              className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer"
+              onClick={() => handleFilter('Flats')}
+            >
+              Flats</div>
+            <div
+              className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer"
+              onClick={() => handleFilter('Shutters')}
+            >
+              Shutters</div>
+            <div
+              className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer"
+              onClick={() => handleFilter('Buildings')}
+            >
+              Building</div>
+            <div
+              className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer"
+              onClick={() => handleFilter('Land')}
+            >
+              Land</div>
           </div>
-          <div className="w-full -mt-[160%] flex">
-            <div className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer">Building</div>
-            <div className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer">Land</div>
-          </div>
-          <div className="bg-red-200 w-full">
-          <div className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer">High to low</div>
-            <div className="flex items-center justify-center my-1 px-2 w-20 hover:bg-green-400 bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer">Land</div>
+          <div className="text-lg pl-2 mt-4">Price</div>
+          <div className="flex flex-wrap categories border-b pb-4 mt-2">
+            <div className="high-to-low flex items-center justify-center my-1 hover:bg-green-400 px-4 py-[1rem] w-auto bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer"
+              onClick={() => handleSortedPropertiesWithPrice('high-to-low')}
+            >High to low</div>
+            <div className="low to high flex items-center justify-center my-1 hover:bg-green-400 px-4 py-[1rem] w-auto bg-green-300 mx-2 h-[30px] rounded-full cursor-pointer"
+              onClick={() => handleSortedPropertiesWithPrice('low-to-high')}
+            >Low to high</div>
+
           </div>
         </div>
-        <div className="main">
-          <div className="search container pl-[20%]">
-            <div className="search-bar mt-12 flex ">
-              {/*  */}
-              <div className="relative flex w-[35%] h-[2.5rem] bg-green-400 rounded-full"> {/*i can adjust this div now*/}
-                <form action="search" className='w-full bg-red-300 h-full rounded-full  '>
-                  <input type="text" className='w-full h-full border-[0.02rem]  border-black px-5 rounded-full outline-[0.1rem] outline-blue-300' />
-                  <button className='absolute bg-gray-200 hover:bg-gray-300 right-[0.25rem] top-[0.29rem] px-2 py-[0.15rem] font-semibold border border-black rounded-full 
-            '>Search</button>
-                </form>
-              </div>
-            </div>
-          </div>
 
-          <div className="card flex flex-wrap w-[80%] px-[9px] mx-10 ">
-            {
-              allData.map((data) => (
+        <div className="main ml-10">
+          <div
+            className={`card flex ${(searchQuery === allData.address || searchQuery === allData.propertyType?.propertyName) ? 'flex-wrap' : 'flex-wrap'} flex-wrap min-w-[50vw] px-[9px] mx-10 `}>
+             {(searchQuery === '' && filteredData.length === 0) ?
+              allData.map(data => (
                 <PropertyCard
                   key={data.propertyId}
                   propertyId={data.propertyId}
@@ -74,9 +128,18 @@ const Properties = () => {
                   address={data.address}
                   area={data.area}
                 />
-              ))}
-
-
+              )) :
+              filteredData.map(data => (
+                <PropertyCard
+                  key={data.propertyId}
+                  propertyId={data.propertyId}
+                  images={data.images && data.images.length > 0 ? `${imageUrl}/${data.images[0].replace('public\\uploads\\', '')}` : ''}
+                  propertyType={data.propertyType.propertyName}
+                  address={data.address}
+                  area={data.area}
+                />
+              ))
+            }
           </div>
         </div>
       </div>
--- frontend/src/pages/admin-dashboard/AdminDashboard.jsx
+++ frontend/src/pages/admin-dashboard/AdminDashboard.jsx
@@ -1,119 +1,158 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUsers, faBuildingUser, faBuildingCircleCheck, faSackDollar } from '@fortawesome/free-solid-svg-icons';
-import { Chart } from 'react-chartjs-2';
-import { Bar, Doughnut, Line } from 'react-chartjs-2';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, ResponsiveContainer, Cell } from 'recharts';
+import axois from 'axios';
+import { Pie } from 'react-chartjs-2';
 
 const AdminDashboard = () => {
-  //   const data = [
-  //     {
-  //       "label": "Properties",
-  //       "value": 2
-  //     },
-  //     {
-  //       "label": "Rents",
-  //       "value": 2
-  //     },
-  //     {
-  //       "label": "Users",
-  //       "value": 2
-  //     }
-  //   ];
-  //   const labels = data.map(item => item.label);
-  //   const values = data.map(item => item.value);
+  const [allUsers, setUsers] = useState([]);
+  const [allProperties, setAllProperties] = useState([]);
+  const [allRents, setAllRents] = useState([]);
+  const [allReportedProperties, setAllReportedProperties] = useState([]);
 
-  //   const barData = {
-  //     labels: labels, // Use the labels array you defined earlier
-  //     datasets: [
-  //       {
-  //         label: 'Data',
-  //         backgroundColor: 'rgba(75,192,192,0.2)',
-  //         borderColor: 'rgba(75,192,192,1)',
-  //         borderWidth: 1,
-  //         hoverBackgroundColor: 'rgba(75,192,192,0.4)',
-  //         hoverBorderColor: 'rgba(75,192,192,1)',
-  //         data: values,
-  //       },
-  //     ],
-  //   };
-  // };
-return (
-  <div className='w-full mx-5 my-5'>
+  const propertyTypeData = [
+    { name: 'Rooms', count: 10 },
+    { name: 'Building', count: 6 },
+    { name: 'Flats', count: 8 },
+    { name: 'Shutters', count: 5 },
+    { name: 'Land', count: 12 },
+  ]
 
-    <div className="data-tables flex px-32 mt-12 h-36 w-full rounded-lg ">
-      <div className=' border-1 mx-2 px-14 border drop-shadow shadow-lg bg-blue-200 rounded-lg'>
-        <div className='my-5 mx-[55px]'>
-          <FontAwesomeIcon icon={faUsers}
-            size='3x'
-            style={{ color: 'black', paddingTop: "10px" }}
-            className=' cursor-pointer relative'
-            onClick={() => setOpen(!open)}
-          />
+  const allUserUrl = 'http://localhost:3001/api/admin/allUsers';
+  const allPropertiesUrl = 'http://localhost:3001/api/admin/allProperties';
+  const allRentUrl = 'http://localhost:3001/api/admin/allRents';
+  const allReportedPropertiesUrl = 'http://localhost:3001/api/admin/allReportedProperties';
+
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axois.get(allUserUrl);
+        console.log(response.data)
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching properties:', error);
+      }
+    };
+    const fetchProperties = async () => {
+      try {
+        const response = await axois.get(allPropertiesUrl);
+        console.log(response.data)
+        setAllProperties(response.data);
+      } catch (error) {
+        console.error('Error fetching properties:', error);
+      }
+    };
+    const fetchRentedProperties = async () => {
+      try {
+        const response = await axois.get(allRentUrl);
+        console.log(response.data)
+        setAllRents(response.data);
+      } catch (error) {
+        console.error('Error fetching properties:', error);
+      }
+    };
+    const fetchReportedProperties = async () => {
+      try {
+        const response = await axois.get(allReportedPropertiesUrl);
+        console.log(response.data)
+        setAllReportedProperties(response.data);
+      } catch (error) {
+        console.error('Error fetching properties:', error);
+      }
+    };
+    fetchUsers();
+    fetchProperties();
+    fetchRentedProperties();
+    fetchReportedProperties();
+
+  }, [])
+
+
+  return (
+    <div className='w-full mx-5 my-5'>
+
+      <div className="data-tables flex px-32 mt-12 h-36 w-full rounded-lg ">
+        <div className=' border-1 mx-2 px-14 border drop-shadow shadow-lg bg-blue-200 rounded-lg'>
+          <div className='my-5 mx-[55px]'>
+            <FontAwesomeIcon icon={faUsers}
+              size='3x'
+              style={{ color: 'black', paddingTop: "10px" }}
+              className=' cursor-pointer relative'
+              onClick={() => setOpen(!open)}
+            />
+          </div>
+          <div className=' text-xl '>
+            Total User: {allUsers.length}
+          </div>
         </div>
-        <div className=' text-xl '>
-          Total User: 100
+        <div className='border-1 mx-2 px-14 border drop-shadow shadow-lg bg-green-200 rounded-lg'>
+          <div className='my-5 mx-[75px]'>
+            <FontAwesomeIcon icon={faBuildingUser}
+              size='3x'
+              style={{ color: 'black', paddingTop: "10px" }}
+              className=' cursor-pointer relative'
+              onClick={() => setOpen(!open)}
+            />
+          </div>
+          <div className='text-xl '>
+            Total Properties: {allProperties.length}
+          </div>
+        </div>
+        <div className='border-1 mx-2 px-14 border drop-shadow shadow-lg bg-red-200 rounded-lg'>
+          <div className='my-5 mx-[75px]'>
+            <FontAwesomeIcon icon={faBuildingCircleCheck}
+              size='3x'
+              style={{ color: 'black', paddingTop: "10px" }}
+              className=' cursor-pointer relative'
+              onClick={() => setOpen(!open)}
+            />
+          </div>
+          <div className='text-xl mx-9'>
+            Total Rents: {allRents.length}
+          </div>
+        </div>
+        <div className='border-1 mx-2 px-14 border drop-shadow shadow-lg bg-slate-200 rounded-lg'>
+          <div className='my-5 mx-[75px]'>
+            <FontAwesomeIcon icon={faSackDollar}
+              size='3x'
+              style={{ color: 'black', paddingTop: "10px" }}
+              className=' cursor-pointer relative'
+              onClick={() => setOpen(!open)}
+            />
+          </div>
+          <div className='text-xl '>
+            Total Revenue: 2000
+          </div>
         </div>
       </div>
-      <div className='border-1 mx-2 px-14 border drop-shadow shadow-lg bg-green-200 rounded-lg'>
-        <div className='my-5 mx-[75px]'>
-          <FontAwesomeIcon icon={faBuildingUser}
-            size='3x'
-            style={{ color: 'black', paddingTop: "10px" }}
-            className=' cursor-pointer relative'
-            onClick={() => setOpen(!open)}
-          />
+      <div className="graph-div flex  mt-24 -mx-3 bg-green-100 h-[50vh] ">
+        <div className='pie-chart-graph  mx-4 w-full'>
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+              <Pie data={propertyTypeData} dataKey="count" cx="50%" cy="50%" outerRadius={60} fill="#8884d8">
+                {propertyTypeData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={`#${index}0000`} />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
-        <div className='text-xl '>
-          Total Properties: 2000
-        </div>
-      </div>
-      <div className='border-1 mx-2 px-14 border drop-shadow shadow-lg bg-red-200 rounded-lg'>
-        <div className='my-5 mx-[75px]'>
-          <FontAwesomeIcon icon={faBuildingCircleCheck}
-            size='3x'
-            style={{ color: 'black', paddingTop: "10px" }}
-            className=' cursor-pointer relative'
-            onClick={() => setOpen(!open)}
-          />
-        </div>
-        <div className='text-xl mx-9'>
-          Total Rents: 2000
-        </div>
-      </div>
-      <div className='border-1 mx-2 px-14 border drop-shadow shadow-lg bg-slate-200 rounded-lg'>
-        <div className='my-5 mx-[75px]'>
-          <FontAwesomeIcon icon={faSackDollar}
-            size='3x'
-            style={{ color: 'black', paddingTop: "10px" }}
-            className=' cursor-pointer relative'
-            onClick={() => setOpen(!open)}
-          />
-        </div>
-        <div className='text-xl '>
-          Total Revenue: 2000
+        <div className='bar-graph w-1/2 px-[2%] py-10 bg-blue-300'>
+          <BarChart width={550} height={300} data={propertyTypeData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#f5f5f5" />
+          </BarChart>
         </div>
       </div>
     </div>
-    <div className="graph-div flex mt-24 -mx-3 bg-green-100 h-[50vh] ">
-      <div className='pie-chart-graph bg-red-300 mx-4 w-full'>
-        {/* <Bar
-          data={barData}
-          options={{
-            maintainAspectRatio: false, // Disable aspect ratio
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
-              }]
-            }
-          }}
-        /> */}
-      </div>
-      <div className='bar-graph px-[23%] bg-blue-300'>fir</div>
-    </div>
-  </div>
-)
+  )
 }
 
 export default AdminDashboard
